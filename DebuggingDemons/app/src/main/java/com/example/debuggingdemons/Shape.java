38a072a519be3b89431d2a3328776c2d77638785
Kele778 <100812664+Kele778@users.noreply.github.com>
2023-10-04 18:38:20 -0600

    Shape class

---
+++ DebuggingDemons/app/src/main/java/com/example/debuggingdemons/Shape.java
@@ -0,0 +1,12 @@
+package com.example.debuggingdemons;
+
+public abstract class Shape {
+    private int x;
+    private int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
